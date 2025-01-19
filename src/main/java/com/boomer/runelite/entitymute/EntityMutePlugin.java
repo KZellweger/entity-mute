8b2c0c7d4b4a9b2ceb4477b8294cf0f4b7653bba
boomerfusel <47775177+boomer-fusel@users.noreply.github.com>
2025-01-19 10:15:09 +0100

    find possible sound origin game objects on Area sounds

--- src/main/java/com/boomer/runelite/entitymute/EntityMutePlugin.java
+++ src/main/java/com/boomer/runelite/entitymute/EntityMutePlugin.java
@@ -1,116 +1,170 @@
 package com.boomer.runelite.entitymute;
 
+import com.google.common.base.MoreObjects;
 import com.google.inject.Provides;
-import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
+import net.runelite.api.events.AreaSoundEffectPlayed;
 import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.MenuEntryAdded;
-import net.runelite.api.events.MenuOpened;
+import net.runelite.api.events.SoundEffectPlayed;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.ItemManager;
-import net.runelite.client.input.MouseManager;
-import net.runelite.client.menus.MenuManager;
+import net.runelite.client.game.NpcUtil;
+import net.runelite.client.game.npcoverlay.HighlightedNpc;
+import net.runelite.client.game.npcoverlay.NpcOverlayService;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
 
+import javax.inject.Inject;
+import java.awt.*;
 import java.util.*;
+import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Entity-Mute"
+        name = "Entity-Mute"
 )
-public class EntityMutePlugin extends Plugin
-{
-	@Inject
-	private Client client;
+public class EntityMutePlugin extends Plugin {
+    private final Map<NPC, HighlightedNpc> highlightedNpcs = new HashMap<>();
+    private final Function<NPC, HighlightedNpc> isHighlighted = highlightedNpcs::get;
+    @Inject
+    private Client client;
+    @Inject
+    private EntityMuteConfig config;
+    @Inject
+    private ItemManager itemManager;
+    @Inject
+    private OverlayManager overlayManager;
+    @Inject
+    private NpcOverlayService npcOverlayService;
+    @Inject
+    private NpcUtil npcUtil;
 
-	@Inject
-	private EntityMuteConfig config;
-	@Inject
-	private MouseManager mouseManager;
-	@Inject
-	private ItemManager itemManager;
-	private EntityMuteMouseListener mouseListener;
+    @Subscribe
+    public void onGameStateChanged(GameStateChanged gameStateChanged) {
+        if (gameStateChanged.getGameState() == GameState.LOGGED_IN) {
+            client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+        }
+    }
 
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("Example started!");
-		mouseListener = new EntityMuteMouseListener(this);
-		mouseManager.registerMouseListener(mouseListener);
-	}
+    @Subscribe
+    public void onSoundEffectPlayed(SoundEffectPlayed soundEffectPlayed) {
+        final Actor actor = soundEffectPlayed.getSource();
 
-	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("Example stopped!");
-		mouseManager.unregisterMouseListener(mouseListener);
-	}
-
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
-	}
-
-	@Subscribe
-	public void onMenuOpened(MenuOpened event) {
-
-		var sortedEntries = existingEntriesByTarget(event.getMenuEntries());
-
-		sortedEntries.entrySet().forEach(entry -> {
-			MenuEntry templateEntry = entry.getValue().get(0);
-			client.getMenu()
-					.createMenuEntry(-1)
-					.setOption("Toggle Mute")
-					.setTarget(entry.getKey())
-					.setType(MenuAction.RUNELITE)
-					.onClick(it -> {
-						determineEntity(it);
-					});
-		});
-		log.info("Menu opened");
-	}
-
-	private Map<String, List<MenuEntry>> existingEntriesByTarget(MenuEntry[] entries){
-		return Arrays.stream(entries)
-				.filter(menuEntry -> menuEntry.getTarget() != null && !menuEntry.getTarget().isEmpty())
-				.collect(Collectors.groupingBy(MenuEntry::getTarget));
-	}
-
-	private void determineEntity(MenuEntry menuEntry) {
-		int itemId = menuEntry.getItemId();
-		ItemComposition itemComposition = itemManager.getItemComposition(itemId);
-		String name = itemComposition.getName();
-		log.info("Item name: {}", name);
-
-		Optional<NPC> npc = Optional.ofNullable(menuEntry.getNpc());
-		if(npc.isPresent()){
-			log.info("We have an NPC");
-		}
-
-		Optional<Actor> actor = Optional.ofNullable(menuEntry.getActor());
-		if(actor.isPresent()){
-			log.info("We have an Actor");
-		}
-
-		Optional<Player> player = Optional.ofNullable(menuEntry.getPlayer());
-		if(player.isPresent()){
-			log.info("We have an Actor");
-		}
+        Player localPlayer = client.getLocalPlayer();
 
 
-	}
+        if (actor == null) {
+            log.warn("Sound effect source not an Actor: {}", soundEffectPlayed.getSoundId());
+            log.warn("Sound effect : {}", MoreObjects.toStringHelper(soundEffectPlayed).add("id", soundEffectPlayed.getSoundId()));
+            return;
+        }
+        if (actor instanceof NPC) {
+            final NPC npc = (NPC) actor;
+            highlightedNpcs.put(npc, highlightedNpc(npc));
+            log.debug("Highlighted npc: {}", MoreObjects.toStringHelper(npc).add("id", npc.getId()));
+        }
+        if (actor instanceof Player) {
+            final Player player = (Player) actor;
+            log.debug("Highlighted player: {}", MoreObjects.toStringHelper(player).add("id", player.getName()));
+        }
 
-	@Provides
-	EntityMuteConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(EntityMuteConfig.class);
-	}
+        npcOverlayService.rebuild();
+    }
+
+    @Subscribe
+    public void onAreaSoundEffectPlayed(AreaSoundEffectPlayed soundEffectPlayed) {
+
+        final Actor actor = soundEffectPlayed.getSource();
+        String actorString = "None";
+
+        Player localPlayer = client.getLocalPlayer();
+        WorldView playersWorldView = localPlayer.getWorldView();
+        int areaSoundEffectOriginSceneX = soundEffectPlayed.getSceneX();
+        int areaSoundEffectOriginSceneY = soundEffectPlayed.getSceneY();
+
+        Scene scene = playersWorldView.getScene();
+        int z = playersWorldView.getPlane();
+
+        Tile[][][] tiles = scene.getTiles();
+
+        ArrayList<Tile> tilesToCheck = new ArrayList<>();
+        tilesToCheck.add(tiles[z][areaSoundEffectOriginSceneX][areaSoundEffectOriginSceneY]);
+        tilesToCheck.add(tiles[z][areaSoundEffectOriginSceneX - 1][areaSoundEffectOriginSceneY]);
+        tilesToCheck.add(tiles[z][areaSoundEffectOriginSceneX + 1][areaSoundEffectOriginSceneY]);
+        tilesToCheck.add(tiles[z][areaSoundEffectOriginSceneX][areaSoundEffectOriginSceneY - 1]);
+        tilesToCheck.add(tiles[z][areaSoundEffectOriginSceneX][areaSoundEffectOriginSceneY + 1]);
+        tilesToCheck.add(tiles[z][areaSoundEffectOriginSceneX +1 ][areaSoundEffectOriginSceneY + 1]);
+        tilesToCheck.add(tiles[z][areaSoundEffectOriginSceneX - 1 ][areaSoundEffectOriginSceneY - 1]);
+
+        List<GameObject> possibleOriginObjects = tilesToCheck.stream().flatMap(tile -> Arrays.stream(tile.getGameObjects()).filter(Objects::nonNull)).collect(Collectors.toList());
+
+        for(GameObject gameObject : possibleOriginObjects) {
+            log.warn("Found possible Sound origin game object: {}", gameObject.getId());
+        }
+
+        if (actor != null) {
+            String type = "";
+            if (actor instanceof Player) {
+                type = "Player";
+            } else {
+                type = "NPC";
+            }
+            actorString = "Actor: " + MoreObjects.toStringHelper(actor)
+                    .add("type: ", type)
+                    .add("name", actor.getName())
+                    .add("actor sceneX", actor.getLocalLocation().getSceneX())
+                    .add("actor sceneY", actor.getLocalLocation().getSceneY());
+        }
+
+        log.debug("Area Sound effect played: {} actor: {}", MoreObjects
+                        .toStringHelper(soundEffectPlayed)
+                        .add("id", soundEffectPlayed.getSoundId())
+                        .add("sceneX", soundEffectPlayed.getSceneX())
+                        .add("sceneY", soundEffectPlayed.getSceneY())
+                        .add("range", soundEffectPlayed.getRange())
+                        .add("delay", soundEffectPlayed.getDelay()),
+                actorString
+        );
+
+        if (actor instanceof NPC) {
+            final NPC npc = (NPC) actor;
+            highlightedNpcs.put(npc, highlightedNpc(npc));
+            log.debug("Highlighted npc: {}", MoreObjects.toStringHelper(npc).add("id", npc.getId()));
+        }
+        npcOverlayService.rebuild();
+    }
+
+    @Override
+    protected void startUp() throws Exception {
+        log.info("Example started!");
+        npcOverlayService.registerHighlighter(isHighlighted);
+    }
+
+    @Override
+    protected void shutDown() throws Exception {
+        log.info("Example stopped!");
+        npcOverlayService.unregisterHighlighter(isHighlighted);
+    }
+
+    @Provides
+    EntityMuteConfig provideConfig(ConfigManager configManager) {
+        return configManager.getConfig(EntityMuteConfig.class);
+    }
+
+    private HighlightedNpc highlightedNpc(NPC npc) {
+        Color color = Color.PINK;
+        final int npcId = npc.getId();
+        return HighlightedNpc.builder()
+                .npc(npc)
+                .highlightColor(color)
+                .name(true)
+                .render(it -> true)
+                .build();
+    }
+
 }
